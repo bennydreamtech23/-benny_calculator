a2f40542c0cff8ea3b96c75b2e766478907ba819
bennydreamtech23 <uwabunkeonyeijeoma@gmail.com>
2023-04-24 23:11:32 +0100

    fix: the ui of the page

--- src/components/calculator_modal/Calculator_modal.tsx
+++ src/components/calculator_modal/Calculator_modal.tsx
@@ -3,7 +3,9 @@ import {useState} from "react";
 import styles from "./Calculator_modal.module.scss"
 
 function Calculator() {
-  // TODO: implement logic of the calculator interface!
+//TODO: implement the on/off button
+const [isOn, setIsOn] = useState<boolean>(true)
+// TODO: implement logic of the calculator interface!
 const [result, setResult] = useState<string>("");
   const [prevNumber, setPrevNumber] = useState<string>("");
   const [operator, setOperator] = useState<string>("");
@@ -63,33 +65,37 @@ const [result, setResult] = useState<string>("");
     setOperator("");
   };
 
-
+const handleOnClick = () => {
+    setIsOn(!isOn);
+    setResult('');
+  };
+  
   return (
     <div className={styles.calculator}>
       <input data-testid="display" className={styles.displayTab} type="text" disabled value={result}></input>
 
       <div className={styles.btn_container}>
-            <button data-testid="btn-clear" className={`${styles.btn} ${styles.wide}`} onClick={() => handleClearClick()}>ON</button>  
-        <button data-testid="btn-clear" className={`${styles.btn} ${styles.wide}`} onClick={() => handleClearClick()}>C</button>
-        <button data-testid="btn-div" className={styles.btn}onClick={() => handleOperatorClick("/")}>/</button>
+              <button className={`${styles.btn} ${styles.wide}`} onClick={() =>handleOnClick()} >{isOn ? "ON" : "OFF"}</button>
+        <button data-testid="btn-clear" className={`${styles.btn} ${styles.wide}`} onClick={() => handleClearClick()} disabled={!isOn}>C</button>
+        <button data-testid="btn-div" className={styles.btn}onClick={() => handleOperatorClick("/")} disabled={!isOn}>/</button>
 
-        <button data-testid="btn-7" className={styles.btn} onClick={() => handleNumberClick("7")}>7</button>
-        <button data-testid="btn-8" className={styles.btn} onClick={() => handleNumberClick("8")}>8</button>
-        <button data-testid="btn-9" className={styles.btn} onClick={() => handleNumberClick("9")}>9</button>
-        <button data-testid="btn-mul" className={styles.btn} onClick={() => handleOperatorClick("*")}>*</button>
+        <button data-testid="btn-7" className={styles.btn} onClick={() => handleNumberClick("7")} disabled={!isOn}>7</button>
+        <button data-testid="btn-8" className={styles.btn} onClick={() => handleNumberClick("8")} disabled={!isOn}>8</button>
+        <button data-testid="btn-9" className={styles.btn} onClick={() => handleNumberClick("9")} disabled={!isOn}>9</button>
+        <button data-testid="btn-mul" className={styles.btn} onClick={() => handleOperatorClick("*")} disabled={!isOn}>*</button>
 
-        <button data-testid="btn-4" className={styles.btn} onClick={() => handleNumberClick("4")}>4</button>
-        <button data-testid="btn-5" className={styles.btn} onClick={() => handleNumberClick("5")}>5</button>
-        <button data-testid="btn-6" className={styles.btn} onClick={() => handleNumberClick("6")}>6</button>
-        <button data-testid="btn-sub" className={styles.btn} onClick={() => handleOperatorClick("-")}>-</button>
+        <button data-testid="btn-4" className={styles.btn} onClick={() => handleNumberClick("4")} disabled={!isOn}>4</button>
+        <button data-testid="btn-5" className={styles.btn} onClick={() => handleNumberClick("5")} disabled={!isOn}>5</button>
+        <button data-testid="btn-6" className={styles.btn} onClick={() => handleNumberClick("6")} disabled={!isOn}>6</button>
+        <button data-testid="btn-sub" className={styles.btn} onClick={() => handleOperatorClick("-")} disabled={!isOn}>-</button>
 
-        <button data-testid="btn-1" className={styles.btn} onClick={() => handleNumberClick("1")}>1</button>
-        <button data-testid="btn-2" className={styles.btn} onClick={() => handleNumberClick("2")}>2</button>
-        <button data-testid="btn-3" className={styles.btn} onClick={() => handleNumberClick("3")}>3</button>
-        <button data-testid="btn-add" className={styles.btn} onClick={() => handleOperatorClick("+")}>+</button>
+        <button data-testid="btn-1" className={styles.btn} onClick={() => handleNumberClick("1")} disabled={!isOn}>1</button>
+        <button data-testid="btn-2" className={styles.btn} onClick={() => handleNumberClick("2")} disabled={!isOn}>2</button>
+        <button data-testid="btn-3" className={styles.btn} onClick={() => handleNumberClick("3")} disabled={!isOn}>3</button>
+        <button data-testid="btn-add" className={styles.btn} onClick={() => handleOperatorClick("+")} disabled={!isOn}>+</button>
 
-        <button data-testid="btn-0" className={`${styles.btn} ${styles.wide}`} onClick={() => handleNumberClick("0")}>0</button>
-        <button data-testid="btn-eval" className={styles.btn} onClick={() => handleEqualsClick()}>=</button>
+        <button data-testid="btn-0" className={`${styles.btn} ${styles.wide}`} onClick={() => handleNumberClick("0")} disabled={!isOn}>0</button>
+        <button data-testid="btn-eval" className={styles.btn} onClick={() => handleEqualsClick()} disabled={!isOn}>=</button>
       </div>
     </div>
   );
